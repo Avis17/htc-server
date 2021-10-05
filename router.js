3a50eba6bd7874013a3020146c438102e69e88fa
Avis17 <pvadivelsiva@gmail.com>
2021-10-05 20:02:44 +0530

    eod - 10/05/2021

--- router.js
+++ router.js
@@ -15,6 +15,62 @@ module.exports = function(app) {
           .status(500)
           .send(error);
         });
+      }),
+      app.post("/api/web/addcustomer", function (req, res){
+        let collectionName = req.body.collectionName;
+        let reqdata = req.body.reqdata
+        mongodb.insert(collectionName,reqdata).then(async (result) => {
+          res.send({
+            status: 200,
+            data:
+              "new data is addded successfully for " + collectionName,
+          });
+          return;
+        }, async (error) => {
+          res
+          .status(500)
+          .send({
+            data: error.msg,
+            error: error.err,
+          });
+        });
+      }),
+      app.post("/api/web/getcustomers", function (req, res){
+        let collectionName = req.body.collectionName;
+        let reqdata = req.body.query;
+        mongodb.query(collectionName,reqdata).then(async (result) => {
+          res.status(200).send(result);
+          return;
+        }, async (error) => {
+          res
+          .status(500)
+          .send(error);
+        });
+      }),
+      app.post("/api/web/editcustomer", function (req, res){
+        let collectionName = req.body.collectionName;
+        let data = req.body.data;
+        let query = req.body.query;
+        mongodb.update(collectionName,query,data).then(async (result) => {
+          res.status(200).send(result);
+          return;
+        }, async (error) => {
+          res
+          .status(500)
+          .send(error);
+        });
+      }),
+      app.post("/api/web/deletecustomer", function (req, res){
+        let collectionName = req.body.collectionName;
+        let reqdata = req.body.query;
+        mongodb.deleteItem(collectionName,reqdata).then(async (result) => {
+          res.status(200).send({result:result, status:200});
+          return;
+        }, async (error) => {
+          res
+          .status(500)
+          .send(error);
+        });
       })
 }
 
