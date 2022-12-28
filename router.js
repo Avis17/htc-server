a57178cb316dc64d804ce28b0da5c6d81b0ce9f3
Avis17 <39846391+Avis17@users.noreply.github.com>
2022-12-28 17:35:52 +0530

    added edit and delete route

--- router.js
+++ router.js
@@ -51,6 +51,34 @@ module.exports = function(app) {
           .send(error);
         });
       })
+      app.post("/api/web/update-user", function (req, res){
+        let collectionName = req.body.collectionName;
+        let data = req.body.data;
+        let query = req.body.query;
+        mongodb.update(collectionName,query,data).then(async (result) => {
+          res.status(200).send({
+            status : 200,
+            data : result
+          });
+          return;
+        }, async (error) => {
+          res
+          .status(500)
+          .send(error);
+        });
+      })
+      app.post("/api/web/delete-user", function (req, res){
+        let collectionName = req.body.collectionName;
+        let reqdata = req.body.query;
+        mongodb.deleteItem(collectionName,reqdata).then(async (result) => {
+          res.status(200).send({data:result, status:200});
+          return;
+        }, async (error) => {
+          res
+          .status(500)
+          .send(error);
+        });
+      })
 }
 
 
